45161abc867b9b7205c4275f66f95381e88ee8fd
Alessandro Tiberia <tiberia7030@gmail.com>
2026-05-12 14:45:19 +0200

    creato components homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 import './App.css'
 
 function App() {
 
   return <>
-      
+      <Navbar />
+      <Home />
     </>
   
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+
+    return <>
+        <div>
+            <h1>Benvenuto nel nostro sito web</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consectetur tempora dolore provident, veniam ipsum odio fugiat! Molestiae ducimus eligendi, omnis itaque non, maxime ad quaerat, consequuntur nulla animi blanditiis!</p>
+        </div>
+    </>
+}
+export default Home;
