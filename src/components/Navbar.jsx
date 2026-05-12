9a6f2eae167cbce893877038020a7ebb811a7053
Alessandro Tiberia <tiberia7030@gmail.com>
2026-05-12 21:48:11 +0200

    esercizio completato

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/chi-siamo">Chi siamo</Link></li>
-            <li><Link to="/Info">contattaci</Link></li>
+            <li><Link to="/prodotti">i nostri prodotti</Link></li>
         </ul>
     </nav>
     </>
