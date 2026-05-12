1d841616e932a626e8c00470b17a2e9c81be05a1
Alessandro Tiberia <tiberia7030@gmail.com>
2026-05-12 15:29:00 +0200

    fetch API

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function Products() {
 
+    //ARRAY vuoto dobve mettere prodotti
+    const [Products, setProducts] = useState([]);
+    
+    useEffect(() => {
+
+        fetch('https://fakestoreapi.com/products')
+        //then aspetta il server che risponde e manda poi la risposta formato json
+        .then(response => response.json())
+        .then(data => { // convertiti i dati li mettiamo nella variabile di stato
+            setProducts(data);
+        });
+    }, []);
+
     return <>
+
+
     </>
 }
 export default Products;
